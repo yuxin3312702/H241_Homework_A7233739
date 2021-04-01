0f791f31bf2f1938f319a16795f791bb38ce5a7b
Your Name <you@example.com>
2021-04-01 19:54:10 +0800

    second commit

--- A7233739/App_Start/BundleConfig.cs
+++ A7233739/App_Start/BundleConfig.cs
@@ -20,7 +20,8 @@ namespace A7233739
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
